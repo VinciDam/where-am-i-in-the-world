41068daa8a9cfcc2fe60583c9e75156e43310133
Vinci Damiani <vinci.damiani@gmail.com>
2025-06-04 16:08:13 +0200

    Add image timing

--- renderers/image.js
+++ renderers/image.js
@@ -1,14 +1,23 @@
 export function showImage(item, next, contentEl, activeTimeouts, lastWasValueRef) {
-    contentEl.appendChild(document.createElement("br"));
-    const img = document.createElement("img");
-    img.src = item.image;
-    img.alt = item.alt || "";
-    img.className = "content-image";
-    img.style.display = "block";
-    img.style.margin = "1rem 0";
-    if (item.width) img.style.width = item.width;
-    contentEl.appendChild(img);
-    lastWasValueRef.current = false;
-    activeTimeouts.push(setTimeout(next, 100));
+  // Clear previous image if you want to replace it
+  const existingImg = contentEl.querySelector(".content-image");
+  if (existingImg) {
+    contentEl.removeChild(existingImg);
   }
-  
+
+  // Create and insert the new image
+  const img = document.createElement("img");
+  img.src = item.image;
+  img.alt = item.alt || "";
+  img.className = "content-image";
+  img.style.display = "block";
+  img.style.margin = "1rem 0";
+  if (item.width) img.style.width = item.width;
+
+  contentEl.appendChild(img);
+  lastWasValueRef.current = false;
+
+  // Delay next() based on replaceAfter
+  const delay = item.replaceAfter || 100;
+  activeTimeouts.push(setTimeout(next, delay));
+}
