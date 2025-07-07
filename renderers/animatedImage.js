2066c04adf7584db4341ed0277049474b316cc42
Vinci Damiani <vinci.damiani@gmail.com>
2025-07-07 16:09:56 +0200

    Factor createAnimatedImage

--- renderers/animatedImage.js
+++ renderers/animatedImage.js
@@ -1,19 +1,20 @@
-export function showAnimatedImage(item, next, contentEl, activeTimeouts, lastWasValueRef) {
-    const existingImg = contentEl.querySelector(".animated-image");
-
-    if (existingImg) {
-        existingImg.remove();
-    }
-
+function createAnimatedImg(src, alt, width) {
     const img = document.createElement("img");
-    img.src = item.animatedImage;
-    img.alt = item.alt || "";
+    img.src = src;
+    img.alt = alt || "";
     img.className = "animated-image";
     img.style.position = "absolute";
     img.style.display = "block";
-    img.style.maxWidth = item.width || "200px";
-    img.style.transform = "translate(-50%, -50%)";
+    img.style.maxWidth = width || "200px";
+    return img;
+}
 
+export function showAnimatedImage(item, next, contentEl, activeTimeouts, lastWasValueRef) {
+    const existingImg = contentEl.querySelector(".animated-image");
+    if (existingImg) existingImg.remove();
+
+    const img = createAnimatedImg(item.animatedImage, item.alt, item.width);
+    img.style.transform = "translate(-50%, -50%)";
     // Start positions
     img.style.left = item.startX || "0%";
     img.style.top  = item.startY  || "50%";
@@ -26,8 +27,6 @@ export function showAnimatedImage(item, next, contentEl, activeTimeouts, lastWas
 
         // default animation distance
         const duration = item.duration || 5000;
-
-        // ensure the duck *exits* fully:
         const endX = item.endX || "130%";
         const endY = item.endY || "50%";
 
@@ -45,13 +44,7 @@ export function showScalingImage(item, next, contentEl, activeTimeouts, lastWasV
     const existingImg = contentEl.querySelector(".animated-image");
     if (existingImg) existingImg.remove();
 
-    const img = document.createElement("img");
-    img.src = item.scalingImage;
-    img.alt = item.alt || "";
-    img.className = "animated-image";
-    img.style.position = "absolute";
-    img.style.display = "block";
-    img.style.maxWidth = item.width || "200px";
+    const img = createAnimatedImg(item.animatedImage, item.alt, item.width);
     img.style.transformOrigin = "center center";
     
     // starting location
